fd965a3794116b71a164fdc60227b7d1eb3e23c6
RubenLopezM <rublopmag@hotmail.com>
2021-11-18 16:18:36 +0000

    APIs

--- src/main.py
+++ src/main.py
@@ -6,9 +6,10 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +31,96 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+@app.route('/user/<int:id>', methods=['GET'])
+def getuser_byId(id):
+    user= User.get_byid(id) 
+
+    if user:
+        return jsonify(user.to_dict()),200
+    
+    return jsonify({'error':'User not found'}), 404
+
+
+@app.route('/user/<int:id>/task', methods=['GET'])
+def get_usertasks(id):
+    user= User.get_byid(id) 
+
+    if user:
+        tasks= Task.get_byuser(id)
+        if tasks:
+            all_tasks= [task.to_dict() for task in tasks]
+            return jsonify(all_tasks), 200
+
+      
+        
+        
+
+    return jsonify({'error':'User not found'}), 404
+
+
+@app.route('/user/<int:id>/task', methods=['POST'])
+def create_usertask(id):
+    user= User.get_byid(id) 
+    text= request.json.get('text',None) #En Postman tendríamos que indicar text en el body
+    if user and text:
+        task=Task(text=text, done=False, user_id=id)
+        task.create()
+        return jsonify(task.to_dict()), 201
+    
+
+    return jsonify({'error':'Task not found'}), 404
+
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def deleteuser(id):
+    user= User.get_byid(id) 
+
+    if user:
+        user= user.delete()
+        return jsonify(user.to_dict()), 200
+    
+    return jsonify({'error':'User not found'}), 404
+
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users= User.get_all()
+    all_users = [user.to_dict() for user in users] #Pasamos los elementos de la lista a diccionarios
 
-    return jsonify(response_body), 200
+    return jsonify(all_users), 200
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+
+    newuser= request.json.get('user', None)
+
+    if not newuser:
+        return jsonify({'error':'Missing data'}),400
+
+    user= User(name=newuser,is_active=True)
+    
+    user_created= user.create()
+    return jsonify(user_created.to_dict()),201
+
+
+@app.route('/user/<int:id>/task/<int:id_task>', methods=['DELETE'])
+def deletetask_user(id,id_task):
+    user= User.get_byid(id) 
+
+    if user:
+        task= Task.gettask_id(id_task)
+        
+        if task:
+            task=task.delete()
+            return jsonify(task.to_dict()), 200
+        
+        return jsonify({'error':'Task not found'}), 404
+
+    return jsonify({'error':'User not found'}), 404
+    
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,93 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__:'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    children = relationship("Task", backref="parent")
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    is_active= db.Column(db.Boolean(),unique=False, nullable=False)
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User is {self.name}, id:{self.id}'
 
-    def serialize(self):
+    def to_dict(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
             # do not serialize the password, its a security breach
         }
+    
+
+    @classmethod
+    def get_byid(cls,id_user):
+        user= cls.query.filter_by(id=id_user).one_or_none()
+        return user
+    
+
+    @classmethod
+    def get_all(cls):
+        users= cls.query.all()
+        return users
+
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+
+    def delete(self):
+        self.is_active=False
+        db.session.commit()
+        return self
+
+    
+        
+class Task(db.Model):
+    __tablename__:'task'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(120), unique=False, nullable=False)
+    done = db.Column(db.Boolean(),unique=False, nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    
+
+    def __repr__(self):
+        return f'The task is {self.text}, id:{self.id}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "done":self.done
+            # do not serialize the password, its a security breach
+        }
+    
+    @classmethod
+    def get_byuser(cls,user_id):
+        tasks= cls.query.filter_by(user_id=user_id)
+        return tasks
+    
+
+    @classmethod
+    def gettask_id(cls,task_id):
+        task=cls.query.get(task_id)
+        return task
+
+    
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    
+    def delete(self):
+        self.done=True
+        db.session.commit()
+        return self
+    
+
+    
